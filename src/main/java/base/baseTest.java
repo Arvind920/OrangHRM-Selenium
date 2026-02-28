d398e9eea99935b413f9149e21c4a9b7dc17bc7c
Arvind920 <vishwakarmaarvind920@gmail.com>
2026-02-28 13:56:51 +0530

    Added Log4j to log message

--- src/main/java/base/baseTest.java
+++ src/main/java/base/baseTest.java
@@ -4,32 +4,35 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import utils.Log;
 
 public class baseTest {
 
 	protected WebDriver driver;
 	private By LogingButton = By.xpath("//button[@type=\"submit\"]");
-	private  WebDriverWait  wait = new WebDriverWait (driver, Duration.ofSeconds(10));
-		
+	private WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
 	@BeforeMethod
 	public void setup() throws InterruptedException {
+		Log.info("Startign WebDriver........");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
+		
+		Log.info("Navigatting to URL");
 		driver.get("https://opensource-demo.orangehrmlive.com/");
 //		WebElement element = wait.until(
 //		        ExpectedConditions.visibilityOfElementLocated(LogingButton));
 		Thread.sleep(5000);
 	}
-	
+
 	@AfterMethod
 	public void tearDown() {
-		if(driver!= null) {
+		if (driver != null) {
 		}
 	}
 }
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -4,14 +4,17 @@ import org.testng.annotations.Test;
 
 import base.baseTest;
 import pages.loginPage;
+import utils.Log;
 
 public class LoginTest extends baseTest{
 	
 	@Test
 	public void validLogin() {
+		
+		Log.info("Starting Login Test");
 		loginPage loginpage = new loginPage(driver);
 		
-		
+		Log.info("Entring User Details");
 		loginpage.enterUsername("Admin");
 		loginpage.enterPassword("admin123");
 		loginpage.clickLogin();
